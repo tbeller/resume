9348cb749d49dcc52a944474ec28cf7c6904c785
Tom Beller <mr.tom.beller@gmail.com>
2025-01-01 05:11:04 +0000

    Test simple function

--- api/src/functions/visitorCounter.js
+++ api/src/functions/visitorCounter.js
@@ -1,6 +1,6 @@
 const { app } = require('@azure/functions');
 
-app.http('message', {
+app.http('visitorCounter', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
